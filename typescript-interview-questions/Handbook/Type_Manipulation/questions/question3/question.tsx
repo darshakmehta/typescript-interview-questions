a0babf8f6890f1b459e1516e61dd5f0a4cb55a6d
Darshak Mehta <darshak_mehta@live.com>
2024-12-28 08:55:13 +0530

    Typeof operator

---
+++ typescript-interview-questions/Handbook/Type_Manipulation/questions/question3/question.tsx
@@ -0,0 +1,4 @@
+// What is the type of shouldContinue?
+
+let shouldContinue: typeof msgbox("Are you sure you want to continue?");
+console.log(typeof shouldContinue);
---
+++ typescript-interview-questions/Handbook/Type_Manipulation/questions/question3/solution.tsx
@@ -0,0 +1,4 @@
+const msgbox = 'Are you sure you want to continue?';
+const shouldContinue: typeof msgbox = msgbox;
+
+console.log(typeof shouldContinue);
